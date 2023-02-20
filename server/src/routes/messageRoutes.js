4b57f572e81c483cb36a20ebf22df6a68951818a
Hassan <hassan.akhtar44@yahoo.com>
2023-02-20 08:06:33 +0500

    added the individual chat room and community chat rooms

--- server/src/routes/messageRoutes.js
+++ server/src/routes/messageRoutes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import requireAuth from "../middlewares/requireAuth.js";
 import Message from "../models/Message.js";
 import ChatRoom from "../models/ChatRoom.js";
+import Users from "../models/Users.js";
 
 const router = Router();
 
@@ -13,13 +14,16 @@ router.get("/fetch-messages", async (req, res) => {
   });
 });
 router.post("/create-room", async (req, res) => {
-  const { recepient } = req.body;
-  const participents = [req.user, recepient];
-  console.log(participents);
+  const recUser = await Users.findOne({ _id: req.body.recipient._id });
+  console.log(recUser);
+
+  const participents = [req.user._id, recUser._id];
+  console.log("users rooms", participents);
   const resp = await new ChatRoom({
     private: true,
     participents: participents,
   });
+  await resp.save();
   res.status(200).send({ data: resp });
   console.log("chat room", resp);
 });
